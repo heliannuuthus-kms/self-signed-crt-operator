a67c10e46f6c0338bf0fc7d798cde05411b1a568
tangbo <tangbo@lixiang.com>
2023-07-27 13:32:26 +0800

    init

---
+++ internal/controller/clusterissuer_controller.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"context"
+	piv1alpha1api "github.com/heliannuuthus/privateca-issuer/api/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+type ClusterIssuerReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=privateca-issuer.gitlabee.chehejia.com,resources=clusterissuers,verbs=get;list;watch
+// +kubebuilder:rbac:groups=privateca-issuer.gitlabee.chehejia.com,resources=clusterissuers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=privateca-issuer.gitlabee.chehejia.com,resources=clusterissuers/finalizers,verbs=update
+
+func (r *ClusterIssuerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+func (r *ClusterIssuerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&piv1alpha1api.ClusterIssuer{}).
+		Complete(r)
+}
