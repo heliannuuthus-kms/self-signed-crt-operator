42a973e7047ed6bb7c51503f4c650f2f54c05e40
tangbo <tangbo@lixiang.com>
2023-07-27 13:31:43 +0800

    init

---
+++ internal/issuer/util/util.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2020 The cert-manager Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	piv1alpha1api "github.com/heliannuuthus/privateca-issuer/api/v1alpha1"
+)
+
+func GetSpecAndStatus(issuer client.Object) (*piv1alpha1api.IssuerSpec, *piv1alpha1api.IssuerStatus, error) {
+	switch t := issuer.(type) {
+	case *piv1alpha1api.Issuer:
+		return &t.Spec, &t.Status, nil
+	case *piv1alpha1api.ClusterIssuer:
+		return &t.Spec, &t.Status, nil
+	default:
+		return nil, nil, fmt.Errorf("not an issuer type: %t", t)
+	}
+}
+
+func SetReadyCondition(status *piv1alpha1api.IssuerStatus, conditionStatus piv1alpha1api.ConditionStatus, reason, message string) {
+	ready := GetReadyCondition(status)
+	if ready == nil {
+		ready = &piv1alpha1api.IssuerCondition{
+			Type: piv1alpha1api.IssuerConditionReady,
+		}
+		status.Conditions = append(status.Conditions, *ready)
+	}
+	if ready.Status != conditionStatus {
+		ready.Status = conditionStatus
+		now := metav1.Now()
+		ready.LastTransitionTime = &now
+	}
+	ready.Reason = reason
+	ready.Message = message
+
+	for i, c := range status.Conditions {
+		if c.Type == piv1alpha1api.IssuerConditionReady {
+			status.Conditions[i] = *ready
+			return
+		}
+	}
+}
+
+func GetReadyCondition(status *piv1alpha1api.IssuerStatus) *piv1alpha1api.IssuerCondition {
+	for _, c := range status.Conditions {
+		if c.Type == piv1alpha1api.IssuerConditionReady {
+			return &c
+		}
+	}
+	return nil
+}
+
+func IsReady(status *piv1alpha1api.IssuerStatus) bool {
+	if c := GetReadyCondition(status); c != nil {
+		return c.Status == piv1alpha1api.ConditionTrue
+	}
+	return false
+}
